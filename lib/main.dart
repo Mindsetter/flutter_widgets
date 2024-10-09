c89303a2a5a5098b06beb79d334c42830a4d7686
Francis <ebo8630@gmail.com>
2024-10-09 16:40:41 +0000

    add timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/screen/timer.dart';
 import 'package:flutter_widgets/widget/cus_gesturedetector.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Widget Day',
-      home: CusGesDector(),
+      home: TimerScreen(),
     );
   }
 }
---
+++ lib/screen/timer.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatefulWidget {
+  const TimerScreen({super.key});
+
+  @override
+  State<TimerScreen> createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  int timeLeft = 5;
+  void timerStart() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (timeLeft > 0) {
+          timeLeft--;
+        } else {
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              timeLeft == 0 ? 'Done' : timeLeft.toString(),
+              style: const TextStyle(fontSize: 40),
+            ),
+            MaterialButton(
+              color: Colors.red,
+              onPressed: timerStart,
+              child: const Text('Start Now'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
