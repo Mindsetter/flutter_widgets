0ce675910193e2a790c03e076949b43cc7e187a8
Francis <ebo8630@gmail.com>
2024-10-11 20:49:06 +0000

    learn text-style

--- lib/screen/pages/page_one.dart
+++ lib/screen/pages/page_one.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
-class PageOne extends StatelessWidget {
+class PageOne extends StatefulWidget {
   const PageOne({super.key});
+
+  @override
+  State<PageOne> createState() => _PageOneState();
+}
+
+class _PageOneState extends State<PageOne> {
+  final textStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.italic,
+    color: Colors.green[300],
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Text(
+            'Text Styling',
+            style: textStyle,
+          ),
+        ),
       ),
     );
   }
