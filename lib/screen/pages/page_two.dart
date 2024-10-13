51389a644fdc609817dd506ed7e89a18e35fe532
Francis <ebo8630@gmail.com>
2024-10-13 23:00:15 +0000

    add Stack

--- lib/screen/pages/page_two.dart
+++ lib/screen/pages/page_two.dart
@@ -6,8 +6,24 @@ class PageTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.deepPurple,
+      body: Stack(
+        children: [
+          Container(
+            height: 300,
+            width: 300,
+            color: Colors.deepPurple,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.pink,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+        ],
       ),
     );
   }
