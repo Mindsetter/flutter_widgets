0d00af78f080386a20a342d6866af32d5bb2efeb
Francis <ebo8630@gmail.com>
2024-10-24 13:00:38 +0000

    add alert dialog

---
+++ lib/screen/alert_dailogue.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AlertDialogScreen extends StatefulWidget {
+  const AlertDialogScreen({super.key});
+
+  @override
+  State<AlertDialogScreen> createState() => _AlertDialogScreenState();
+}
+
+class _AlertDialogScreenState extends State<AlertDialogScreen> {
+  void _showdialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Do you want to quite?'),
+            content: const Text('Are you sure you want to quite?'),
+            actions: [
+              MaterialButton(
+                onPressed: () {},
+                child: const Text('Ok'),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  return Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          color: Colors.amber.shade300,
+          onPressed: _showdialog,
+          child: const Text('Alert Dailog'),
+        ),
+      ),
+    );
+  }
+}
